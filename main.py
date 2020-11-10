4c2be7f6d5ffe3d467a3b88c33f83c36fdecaf36
Oleh Misarosh <olehmisar@gmail.com>
2020-11-10 17:20:36 +0200

    Add support for multiple voices

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import dotenv
 dotenv.load_dotenv()
 
 from utils import calculate_notifications_interval, connect_to_voice_channel, get_user_voice_channel, parse_time_str, round_to_nearest_5
-import texts
+from texts import texts, reactions
 
 
 @dataclass
@@ -34,6 +34,7 @@ class State:
 
     timer: Optional[TimerData] = None
     tts_engine: pyttsx3.Engine = field(default_factory=lambda: pyttsx3.init())
+    voice_language: str = 'en'
 
     def generate_voice(self, text: str):
         filename = os.path.join(
@@ -59,7 +60,7 @@ class State:
 
             timer.voice_client.stop()
             timer.voice_client.play(
-                self.generate_voice(texts.en.time_is_over))
+                self.generate_voice(texts[self.voice_language].time_is_over))
 
         # Notify if needed
         elif now - timer.last_notified > calculate_notifications_interval(time_left):
@@ -72,7 +73,7 @@ class State:
 
             timer.voice_client.stop()
             timer.voice_client.play(self.generate_voice(
-                texts.en.timer_notification(time_left)))
+                texts[self.voice_language].timer_notification(time_left)))
 
 
 # Global state for each guild
@@ -87,16 +88,17 @@ if __name__ == '__main__':
 
     @bot.command()
     async def start(ctx: commands.Context, duration_str=''):
+        state = states[ctx.guild]
         # Check if user is connected to a voice channel
         voice_channel = get_user_voice_channel(ctx)
         if not voice_channel:
-            await ctx.send(f'{ctx.author.mention} {texts.en.requires_to_be_connected_to_a_voice_channel}')
+            await ctx.send(f'{ctx.author.mention} {texts[state.voice_language].requires_to_be_connected_to_a_voice_channel}')
             return
 
         # Parse duration string
         duration = parse_time_str(duration_str)
         if duration is None:
-            await ctx.send(f'{ctx.author.mention} {texts.en.invalid_duration_str}: {duration_str}')
+            await ctx.send(f'{ctx.author.mention} {texts[state.voice_language].invalid_duration_str}: {duration_str}')
             return
 
         # Set a timer
@@ -109,21 +111,50 @@ if __name__ == '__main__':
             )
 
         await connect_to_voice_channel(ctx, voice_channel)
-        await ctx.message.add_reaction(texts.reactions.ok_hand)
+        await ctx.message.add_reaction(reactions.ok_hand)
         ctx.voice_client.stop()
-        ctx.voice_client.play(state.generate_voice(texts.en.start_timer(duration)),
+        ctx.voice_client.play(state.generate_voice(texts[state.voice_language].start_timer(duration)),
                               after=lambda _: start_timer())
 
     @bot.command()
     async def stop(ctx: commands.Context):
-        await ctx.message.add_reaction(texts.reactions.ok_hand)
+        await ctx.message.add_reaction(reactions.ok_hand)
         state = states[ctx.guild]
         if ctx.voice_client:
             def stop_timer():
                 state.timer = None
             ctx.voice_client.stop()
-            ctx.voice_client.play(state.generate_voice(texts.en.timer_is_stopped),
+            ctx.voice_client.play(state.generate_voice(texts[state.voice_language].timer_is_stopped),
                                   after=lambda _: stop_timer())
+
+    @bot.command()
+    async def voice(ctx: commands.Context, voice_index=None):
+        state = states[ctx.guild]
+        voices = sorted(state.tts_engine.getProperty('voices'),
+                        key=lambda voice: voice.name)
+
+        if voice_index is not None:
+            # Set voice
+            try:
+                voice_index = int(voice_index)
+                # `voice_index` starts from 1
+                voice = voices[voice_index - 1]
+            except (IndexError, ValueError):
+                await ctx.send(f'{ctx.author.mention} {texts[state.voice_language].invalid_index}: {voice_index}')
+            else:
+                state.tts_engine.setProperty('voice', voice.id)
+                await ctx.send(f'{ctx.author.mention} {texts[state.voice_language].voice_set_successfully}')
+                for language_code, text in texts.items():
+                    if text._language_name.lower() in voice.name.lower():
+                        state.voice_language = language_code
+                        await ctx.send(f'{ctx.author.mention} {texts[state.voice_language].language_changed_to} {state.voice_language}')
+                        break
+
+        else:
+            # Show available voices
+            message = '\n'.join(f'{i}. {voice.name}'
+                                for i, voice in enumerate(voices, start=1))
+            await ctx.send(message)
 
     async def update_timers():
         await bot.wait_until_ready()
--- texts.py
+++ texts.py
@@ -3,6 +3,8 @@ from utils import to_minutes_seconds
 
 
 class en:
+    _language_name = 'english'
+
     requires_to_be_connected_to_a_voice_channel = 'you must be connected to a voice channel to use this command'
     invalid_duration_str = 'invalid duration'
 
@@ -11,25 +13,66 @@ class en:
 
     @staticmethod
     def start_timer(duration: timedelta):
-        return f'setting a timer for {_to_time_str(duration, include_flat=True)}. 3, 2, 1, GO!'
+        return f'setting a timer for {en._to_time_str(duration, include_flat=True)}. 3, 2, 1, GO!'
 
     @staticmethod
     def timer_notification(time_left: timedelta) -> str:
-        return f'{_to_time_str(time_left)} left'
+        return f'{en._to_time_str(time_left)} left'
+
+    invalid_index = 'invalid index'
+    voice_set_successfully = 'voice was set successfully'
+    language_changed_to = 'language was changed to'
+
+    @staticmethod
+    def _to_time_str(time: timedelta, include_flat=False) -> str:
+        minutes, seconds = to_minutes_seconds(time)
+        ret = ''
+        if minutes != 0:
+            ret += f'{minutes} {"minute" if minutes == 1 else "minutes"}'
+
+        if minutes == 0 or seconds != 0:
+            ret += f'{seconds} {"second" if seconds == 1 else "seconds"}'
+        elif include_flat:
+            ret += 'flat'
+
+        return ret
 
 
-def _to_time_str(time: timedelta, include_flat=False) -> str:
-    minutes, seconds = to_minutes_seconds(time)
-    ret = ''
-    if minutes != 0:
-        ret += f'{minutes} {"minute" if minutes == 1 else "minutes"}'
+class ru:
+    _language_name = 'russian'
 
-    if minutes == 0 or seconds != 0:
-        ret += f'{seconds} {"second" if seconds == 1 else "seconds"}'
-    elif include_flat:
-        ret += 'flat'
+    requires_to_be_connected_to_a_voice_channel = 'вы должны быть присоединены к голосовому чату, чтобы использовать эту команду'
+    invalid_duration_str = 'неверное время'
 
-    return ret
+    time_is_over = 'время вышло'
+    timer_is_stopped = 'таймер остановлен'
+
+    @staticmethod
+    def start_timer(duration: timedelta):
+        return f'запускаю таймер на {ru._to_time_str(duration)}. 3, 2, 1, Время пошло!'
+
+    @staticmethod
+    def timer_notification(time_left: timedelta) -> str:
+        return f'осталось {ru._to_time_str(time_left)}'
+
+    @staticmethod
+    def _to_time_str(time: timedelta) -> str:
+        minutes, seconds = to_minutes_seconds(time)
+        ret = ''
+        if minutes != 0:
+            ret += f'{minutes} {"минута" if minutes == 1 else "минут"}'
+
+        if minutes == 0 or seconds != 0:
+            ret += f'{seconds} {"секунда" if seconds == 1 else "секунд"}'
+
+        return ret
+
+    invalid_index = 'неверный индекс'
+    voice_set_successfully = 'голос успешно изменен'
+    language_changed_to = 'язык изменен на'
+
+
+texts = {'en': en, 'ru': ru}
 
 
 class reactions:
