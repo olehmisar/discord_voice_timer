6591078654e822f41b41de86bebfdd40fcd0d16e
Oleh Misarosh <olehmisar@gmail.com>
2020-11-10 03:35:36 +0200

    Simplify calculate_notifications_interval

--- utils.py
+++ utils.py
@@ -29,15 +29,14 @@ def round_to_nearest_5(x: int) -> int:
 def calculate_notifications_interval(time_left: timedelta) -> timedelta:
     """Dynamically calculates how much time to wait between notification based on the time left"""
 
-    minutes, seconds = to_minutes_seconds(time_left)
-    if minutes > 5:
+    if time_left > timedelta(minutes=5):
         return timedelta(minutes=1)
 
-    elif minutes > 0:
+    elif time_left > timedelta(minutes=1):
         return timedelta(seconds=30)
 
-    else:  # minutes == 0
-        if seconds > 10:
-            return timedelta(seconds=15)
-        else:  # seconds <= 10
-            return timedelta(seconds=5)
+    elif time_left > timedelta(seconds=15):
+        return timedelta(seconds=15)
+
+    else:  # seconds < 10
+        return timedelta(seconds=5)
