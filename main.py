ccc0474df348f5ead563b17485ffc3e4165c933c
Oleh Misarosh <olehmisar@gmail.com>
2020-11-10 15:45:26 +0200

    Add support for multiple simultaneous guilds

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
+from collections import defaultdict
 from datetime import datetime, timedelta
 import tempfile
-from typing import Optional
+from typing import Dict, Optional
 from dataclasses import dataclass, field
 from uuid import uuid4
 import os
@@ -27,22 +28,55 @@ class TimerData:
     last_notified: datetime = field(default_factory=datetime.now)
 
 
-class state:
-    """Just a namespace for the global state"""
+@dataclass
+class State:
+    """Global state for a guild"""
 
     timer: Optional[TimerData] = None
-    tts_engine = pyttsx3.init()
+    tts_engine: pyttsx3.Engine = field(default_factory=lambda: pyttsx3.init())
+
+    def generate_voice(self, text: str):
+        filename = os.path.join(
+            tempfile.gettempdir(),
+            f'discord_generated_voice_{uuid4()}.mp3'
+        )
+
+        self.tts_engine.save_to_file(text, filename)
+        self.tts_engine.runAndWait()
+        return discord.FFmpegPCMAudio(filename)
+
+    def update_timer(self):
+        timer = self.timer
+        if not timer:
+            return
+
+        now = datetime.now()
+        time_left = timer.duration - (now - timer.start)
+
+        # Stop timer
+        if time_left.seconds <= 0:
+            self.timer = None
+
+            timer.voice_client.stop()
+            timer.voice_client.play(
+                self.generate_voice(texts.en.time_is_over))
+
+        # Notify if needed
+        elif now - timer.last_notified > calculate_notifications_interval(time_left):
+            timer.last_notified = now
+
+            # Round to the nearest 5 seconds when the time is not that important yet
+            if time_left.seconds > 30:
+                time_left = timedelta(
+                    seconds=round_to_nearest_5(time_left.seconds))
+
+            timer.voice_client.stop()
+            timer.voice_client.play(self.generate_voice(
+                texts.en.timer_notification(time_left)))
 
 
-def generate_voice(text: str):
-    filename = os.path.join(
-        tempfile.gettempdir(),
-        f'discord_generated_voice_{uuid4()}.mp3'
-    )
-
-    state.tts_engine.save_to_file(text, filename)
-    state.tts_engine.runAndWait()
-    return discord.FFmpegPCMAudio(filename)
+# Global state for each guild
+states: Dict[discord.Guild, State] = defaultdict(State)
 
 
 if __name__ == '__main__':
@@ -66,6 +100,8 @@ if __name__ == '__main__':
             return
 
         # Set a timer
+        state = states[ctx.guild]
+
         def start_timer():
             state.timer = TimerData(
                 duration=duration,
@@ -75,52 +111,26 @@ if __name__ == '__main__':
         await connect_to_voice_channel(ctx, voice_channel)
         await ctx.message.add_reaction(texts.reactions.ok_hand)
         ctx.voice_client.stop()
-        ctx.voice_client.play(generate_voice(texts.en.start_timer(duration)),
+        ctx.voice_client.play(state.generate_voice(texts.en.start_timer(duration)),
                               after=lambda _: start_timer())
 
     @bot.command()
     async def stop(ctx: commands.Context):
         await ctx.message.add_reaction(texts.reactions.ok_hand)
+        state = states[ctx.guild]
         if ctx.voice_client:
             def stop_timer():
                 state.timer = None
             ctx.voice_client.stop()
-            ctx.voice_client.play(generate_voice(texts.en.timer_is_stopped),
+            ctx.voice_client.play(state.generate_voice(texts.en.timer_is_stopped),
                                   after=lambda _: stop_timer())
 
     async def update_timers():
         await bot.wait_until_ready()
         while not bot.is_closed():
+            for state in states.values():
+                state.update_timer()
             await asyncio.sleep(1)
-
-            timer = state.timer
-            if not timer:
-                continue
-
-            now = datetime.now()
-            time_left = timer.duration - (now - timer.start)
-
-            # Stop timer
-            if time_left.seconds <= 0:
-                state.timer = None
-
-                timer.voice_client.stop()
-                timer.voice_client.play(
-                    generate_voice(texts.en.time_is_over))
-
-            # Notify if needed
-            elif now - timer.last_notified > calculate_notifications_interval(time_left):
-                timer.last_notified = now
-
-                # Round to the nearest 5 seconds when the time is not that important yet
-                if time_left.seconds > 30:
-                    time_left = timedelta(
-                        seconds=round_to_nearest_5(time_left.seconds))
-
-                timer.voice_client.stop()
-                timer.voice_client.play(generate_voice(
-                    texts.en.timer_notification(time_left)))
-
     bot.loop.create_task(update_timers())
 
     bot.run(os.getenv('DISCORD_BOT_TOKEN'))
